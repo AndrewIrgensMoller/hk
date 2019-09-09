ffcc5d9a1343abe439c0a8e9f7142f8a92450d50
pedro <pmcpalma@gmail.com>
2019-09-09 21:46:12 +0200

    added room controller, repository and model. added html add-room

--- src/main/java/com/happykealifornia/hk/Model/Room.java
+++ src/main/java/com/happykealifornia/hk/Model/Room.java
@@ -1,4 +1,43 @@
 package com.happykealifornia.hk.Model;
 
 public class Room {
+    //variables
+    private int room_id;
+    private String hotel;
+    private int room_type;
+
+    //constructor
+
+    public int getRoom_id() {
+        return room_id;
+    }
+
+    public String getHotel() {
+        return hotel;
+    }
+
+    public int getRoom_type() {
+        return room_type;
+    }
+
+    public void setRoom_id(int room_id) {
+        this.room_id = room_id;
+    }
+
+    public void setHotel(String hotel) {
+        this.hotel = hotel;
+    }
+
+    public void setRoom_type(int room_type) {
+        this.room_type = room_type;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "room_id=" + room_id +
+                ", hotel='" + hotel + '\'' +
+                ", room_type=" + room_type +
+                '}';
+    }
 }
